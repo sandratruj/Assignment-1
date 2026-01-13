add6ad82e2943b17b3bf238ee4ecfdce550292b8
sandratruj <strujillo33@student.rccd.edu>
2026-01-13 06:02:42 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() { 
+
+  string name = "Sandra Trujillo";
+  string goals = "My goal is to learn to code and go on to study code further";
+  string interests = "I am interested in reading and dancing"; 
+  string dreams = "My dream is to become an aerospace engineer";
+
+
+  cout << "Name: " << name << endl;
+  cout << "Goals: " << goals << endl;
+  cout << "Interests: " << interests << endl;
+  cout << "Dreams: " << dreams << endl;
+
+
+  cout << "Year 1: I see myself studying echanical engineering at UCI" << endl;
+  cout << "Year 3: I see myself graduating from UCI and starting my career" << endl;
+  cout << "Year 5: I see myself building my career and getting married" << endl;
+  cout << "Year 7: I see myself starting my family and having kids" << endl;
+
+  return 0;
+}
